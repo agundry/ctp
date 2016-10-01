c38401e2574e8c32416272c76588fd84235cab08
agundry <abgundry@gmail.com>
2016-10-01 16:55:17 -0500

    Login/Register Links

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import AppHeader from './layout/AppHeader';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    showUserNav(){
+        return  <ul className="nav navbar-nav navbar-right">
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Register</a></li>
+        </ul>;
+    }
+
     render() {
         return (
             <div className="app-container">
-                Hello, world.
+                <AppHeader appTitle="Chicken Tender Party" userNav={this.showUserNav()}/>
                 <main className="container">
                     {this.props.content}
                 </main>
